40d3f797ef6828659471b58d376c887ef26c942b
mcfloundinho <mcfloundinho@gmail.com>
2016-03-14 11:24:15 +0800

    add dummy ai (move blindly)

---
+++ dummy.cpp
@@ -0,0 +1,9 @@
+﻿#include "teamstyle17.h"
+#include <iostream>
+
+void AIMain() {
+	const Status* status = GetStatus();
+	int self_id = status->objects[0].id;
+	std::cout << "id: " << self_id << std::endl;
+	Move(self_id, { 100, 0, 0 });
+}
