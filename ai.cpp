b9f643a0cf7a59e92f29a3271a0865ce89ac737b
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-03-17 18:48:19 +0800

    Version 2.3.0
    Low Level Code Changed for the final game
    Can get access to the 2,or 3 or more objects controlled by AI
    please check if each function is able to use 
    the low level will keep this frame, unless there is a great hit
    best wishes
    Zero Weight

--- ai.cpp
+++ ai.cpp
@@ -39,43 +39,59 @@ class AR_GAME {
 private:
 	const Map* map;
 	int myteam;
-	PlayerObject my;
+	PlayerObject GET (int);
 public:
+	int number;
 	AR_GAME();
-	int MoveInSPEED(AR_VECTOR);
-	int MoveTo(AR_VECTOR);
+	int MoveInSPEED(AR_VECTOR, int i = 0);
+	int MoveTo(AR_VECTOR,int i=0 );
 	Object operator[](int);
-	int cost(SkillType);
-	int Update(SkillType);
-	int long_attack(Object);
-	int short_attack();
-	int dash();
-	int shield();
-	int health();
-	int maxhealth();
-	int vision();
-	double radius();
+	int cost(SkillType,int i=0);
+	int Update(SkillType,int i=0);
+	int long_attack(Object,int i=0);
+	int short_attack(int i=0);
+	int dash(int i=0);
+	int shield(int i=0);
+	int health(int i=0);
+	int maxhealth(int i=0);
+	int vision(int i=0);
+	double radius(int i=0);
 	double radius(Object);
-	AR_VECTOR speed();
-	AR_VECTOR position();
+	AR_VECTOR speed(int i=0);
+	AR_VECTOR position(int i=0);
 	AR_VECTOR position(Object);
-	int long_attack_casting();
+	int long_attack_casting(int i=0);
 	int long_attack_casting(Object);
-	int shield_time();
+	int shield_time(int i=0);
 	int shield_time(Object);
-	int dash_time();
-	int ability();
-	int skill_level(SkillType);
-	int skill_cd(SkillType);
+	int dash_time(int i=0);
+	int ability(int i=0);
+	int skill_level(SkillType,int i=0);
+	int skill_cd(SkillType,int i=0);
 	int check(Object);
 };
-
 
 void AIMain() {
 	AR_GAME G;
 	G.MoveTo(AR_VECTOR(0,0,0));
+	G.ability();
+	G.check(G[0]);
+	G.cost(DASH);
+	G.dash(2);
+	G.dash_time();
+	G.health(1);
+	G.long_attack(G[1]);
+	G.long_attack_casting();
+	G.maxhealth();
+	G.long_attack_casting(G[1]);
+	G.number;
+	G.position(G[3]);
+	G.position(3);
+	G.position();
+	G.radius();
+	G.skill_cd(HEALTH_UP);
+	G.shield();
 }
-
 
 AR_VECTOR::AR_VECTOR() {
 	x = (kMapSize >> 1);
@@ -159,29 +175,36 @@ AR_GAME::AR_GAME()
 {
 	map = GetMap();
 	myteam = GetStatus()->team_id;
-	my = GetStatus()->objects[0];
+	number = GetStatus()->objects_number;
 }
-int AR_GAME::MoveInSPEED(AR_VECTOR P)
+int AR_GAME::MoveInSPEED(AR_VECTOR P, int i)
 {
-	if (!(P == my.speed)) {
-		Move(this->my.id, P.cast());
+	PlayerObject p = GET(i);
+	if (!(P == p.speed)) {
+		Move(p.id, P.cast());
 		return 0;
 	}
 	else return -1;
 }
-int AR_GAME::MoveTo(AR_VECTOR p)
+int AR_GAME::MoveTo(AR_VECTOR p,int i)
 {
+	PlayerObject my = GET(i);
 	if (p == my.pos) return -1;
 	double k = ((kMaxMoveSpeed + kDashSpeed[my.skill_level[DASH]]) / (p - my.pos).length());
 	AR_VECTOR a = (k*(p - my.pos));
 	cout << a;
-	return MoveInSPEED(a);
+	return MoveInSPEED(a,i);
 }
 Object  AR_GAME::operator[](int n)
 {
 	return map->objects[n];
 }
-int AR_GAME::cost(SkillType skill) {
+PlayerObject AR_GAME::GET(int n)
+{
+	return GetStatus()->objects[n];
+}
+int AR_GAME::cost(SkillType skill,int i) {
+	PlayerObject my = GET(i);
 	if (my.skill_level[skill]) {
 		return (kBasicSkillPrice[skill] << my.skill_level[skill]);
 	}
@@ -193,15 +216,17 @@ int AR_GAME::cost(SkillType skill) {
 		return(kBasicSkillPrice[skill] << count);
 	}
 }
-int AR_GAME::Update(SkillType skill)
+int AR_GAME::Update(SkillType skill,int i)
 {
-	if (my.ability >= cost(skill)) {
+	PlayerObject my = GET(i);
+	if (my.ability >= cost(skill,i)) {
 		UpgradeSkill(my.id, skill);
 		return 0;
 	}
 	else return -1;
 }
-int AR_GAME::long_attack(Object aim) {
+int AR_GAME::long_attack(Object aim,int i) {
+	PlayerObject my = GET(i);
 	if (my.skill_level[LONG_ATTACK] && my.skill_cd[LONG_ATTACK] == 0
 		&& (AR_VECTOR(my.pos)-AR_VECTOR(aim.pos)).length()<=kLongAttackRange[my.skill_level[LONG_ATTACK]]) {
 		LongAttack(my.id, aim.id);
@@ -210,7 +235,8 @@ int AR_GAME::long_attack(Object aim) {
 	}
 	else return -1;
 }
-int AR_GAME::short_attack() {
+int AR_GAME::short_attack(int i) {
+	PlayerObject my = GET(i);
 	if (my.skill_level[SHORT_ATTACK] && (my.skill_cd[SHORT_ATTACK] == 0)) {
 		ShortAttack(my.id);
 		printf("SHORT ATTACK AT\t%d\tLEVEL\t%d\n", GetTime(), my.skill_level[SHORT_ATTACK]);
@@ -220,7 +246,8 @@ int AR_GAME::short_attack() {
 		return -1;
 	}
 }
-int AR_GAME::shield() {
+int AR_GAME::shield(int i) {
+	PlayerObject my = GET(i);
 	if (my.skill_level[SHIELD] && my.skill_cd[SHIELD] == 0) {
 		Shield(my.id);
 		printf("SHIELD AT\t%d\tLEVEL\t%d\n", GetTime(), my.skill_level[SHIELD]);
@@ -228,83 +255,96 @@ int AR_GAME::shield() {
 	}
 	else return -1;
 }
-int AR_GAME::health()
+int AR_GAME::health(int i)
 {
+	PlayerObject my = GET(i);
 	return my.health;
 }
-int AR_GAME::maxhealth()
+int AR_GAME::maxhealth(int i)
 {
+	PlayerObject my = GET(i);
 	return my.max_health;
 }
-int AR_GAME::vision()
+int AR_GAME::vision(int i)
 {
+	PlayerObject my = GET(i);
 	return my.vision;
 }
-double AR_GAME::radius()
+double AR_GAME::radius(int i)
 {
+	PlayerObject my = GET(i);
 	return my.radius;
 }
 double AR_GAME::radius(Object aim)
 {
 	return aim.radius;
 }
-AR_VECTOR AR_GAME::speed()
+AR_VECTOR AR_GAME::speed(int i)
 {
+	PlayerObject my = GET(i);
 	return AR_VECTOR(my.speed);
 }
-AR_VECTOR AR_GAME::position()
+AR_VECTOR AR_GAME::position(int i)
 {
+	PlayerObject my = GET(i);
 	return AR_VECTOR(my.pos);
 }
 AR_VECTOR AR_GAME::position(Object aim)
 {
 	return AR_VECTOR(aim.pos);
 }
-int AR_GAME::long_attack_casting()
+int AR_GAME::long_attack_casting(int i)
 {
+	PlayerObject my = GET(i);
 	return my.long_attack_casting;
 }
 int AR_GAME::long_attack_casting(Object aim)
 {
 	return aim.long_attack_casting;
 }
-int AR_GAME::shield_time()
+int AR_GAME::shield_time(int i)
 {
+	PlayerObject my = GET(i);
 	return my.shield_time;
 }
 int AR_GAME::shield_time(Object aim)
 {
 	return aim.shield_time;
 }
-int AR_GAME::dash_time()
+int AR_GAME::dash_time(int i)
 {
+	PlayerObject my = GET(i);
 	return my.dash_time;
 }
-int AR_GAME::ability()
+int AR_GAME::ability(int i)
 {
+	PlayerObject my = GET(i);
 	return my.ability;
 }
-int AR_GAME::skill_level(SkillType skill)
+int AR_GAME::skill_level(SkillType skill,int i)
 {
+	PlayerObject my = GET(i);
 	return my.skill_level[skill];
 }
-int AR_GAME::skill_cd(SkillType skill)
+int AR_GAME::skill_cd(SkillType skill,int i)
 {
+	PlayerObject my = GET(i);
 	return my.skill_cd[skill];
 }
 int AR_GAME::check(Object aim)
 {
 	if (aim.type != PLAYER) return aim.type;
 	else {
-		if (aim.team_id == myteam) return -1;
-		else return 100;
+		if (aim.team_id == myteam) return 88;
+		else return 250;
 	}
 }
-int AR_GAME::dash() {
+int AR_GAME::dash(int i) {
+	PlayerObject my = GET(i);
 	if (my.skill_level[DASH] && my.skill_cd[DASH] == 0) {
 		Dash(my.id);
 		printf("%d\tDASH AT\t\t%d\tLEVEL\t%d\n", my.id, GetTime(), my.skill_level[DASH]);
 		return 0;
 	}
 	else return -1;
-}
+} 
