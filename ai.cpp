767ed9ecc45bd8eddacba83330d19ebb4d68a722
DavidYQY <yinqy15@mails.tsinghua.edu.cn>
2016-03-28 22:32:41 +0800

    Added files via upload

--- ai.cpp
+++ ai.cpp
@@ -1,10 +1,9 @@
-#include "teamstyle17.h"
-#include<cmath>
-#include<iostream>
-#include<deque>
-#include<vector>
-#include<list>
-using namespace std;
+﻿#include "teamstyle17.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+typedef Position Vector;
 enum AR_BORDER {
 	NONE = 0,
 	LEFT = 1 << 0,
@@ -14,476 +13,300 @@ enum AR_BORDER {
 	DOWN = 1 << 4,
 	UP = 1 << 5,
 };
-class AR_VECTOR {
-private:
-	double x;
-	double y;
-	double z;
-public:
-	AR_VECTOR(Position);
-	AR_VECTOR();
-	AR_VECTOR(double, double, double);
-	double length();
-	int border(double);
-	Position cast();
-	void operator=(AR_VECTOR&);
-	void operator=(Position);
-	friend double operator^ (AR_VECTOR&, AR_VECTOR&);
-	friend int operator==(const AR_VECTOR&, const AR_VECTOR&);
-	friend AR_VECTOR operator*(const AR_VECTOR&, const AR_VECTOR&);
-	friend AR_VECTOR operator*(const double, const AR_VECTOR&);
-	friend AR_VECTOR operator*(const AR_VECTOR&, const double);
-	friend AR_VECTOR operator+(const AR_VECTOR&, const AR_VECTOR&);
-	friend AR_VECTOR operator-(const AR_VECTOR&, const AR_VECTOR&);
-	friend double operator,(const AR_VECTOR&, const AR_VECTOR&);
-	friend ostream& operator<<(ostream&, AR_VECTOR&);
-};
-class AR_GAME {
-private:
-	const Map* map;
-	int myteam;
-	PlayerObject GET (int);
-public:
-	
-	double distance(Object, int i = 0);
-	int number;
-	int object_number;
-	AR_GAME();
-	int MoveInSPEED(AR_VECTOR, int i = 0);
-	int MoveTo(AR_VECTOR,int i=0 );
-	Object operator[](int);
-	int cost(SkillType,int i=0);
-	int Update(SkillType,int i=0);
-	int long_attack(Object,int i=0);
-	int short_attack(int i=0);
-	int dash(int i=0);
-	int shield(int i=0);
-	int health(int i=0);
-	int maxhealth(int i=0);
-	int vision(int i=0);
-	double radius(int i=0);
-	double radius(Object);
-	AR_VECTOR speed(int i=0);
-	AR_VECTOR position(int i=0);
-	AR_VECTOR position(Object);
-	int long_attack_casting(int i=0);
-	int long_attack_casting(Object);
-	int shield_time(int i=0);
-	int shield_time(Object);
-	int dash_time(int i=0);
-	int ability(int i=0);
-	int skill_level(SkillType,int i=0);
-	int skill_cd(SkillType,int i=0);
-	int check(Object);
-};
+const double eps = 1e-6;
+
+static PlayerObject me[kMaxPlayerObjectNumber];
+static Object opponent[kMaxPlayerObjectNumber];
+static Object boss;
+static int me_number;
+static int opponent_number;
+static int devour_number;
+static int see_boss;
+
+const Map *map;
+
+void Init();
+void Strategy();
+Vector MaximumSpeed(Vector vec);
+int cost(PlayerObject, SkillType);
+void upgrade(PlayerObject, SkillType);
+void long_attack(PlayerObject, Object);
+void short_attack(PlayerObject);
+void shield(PlayerObject);
+void dash(PlayerObject);
+int IsBorder(double d,Position des);
+int IsDevour(double d,Position des);
+int IsBoss(double d,Position des);
+int FBorder(double r);
 
 
+double dist(Position, Position);
+double length(Vector);
+double dot_product(Vector, Vector);
+double ABS(double);
+double SQR(double);
+double POW(double, int);
+Vector Add(Vector a1,Vector a2);
+Vector Minus(Vector a1,Vector a2);
+Vector Multiple(double k,Vector a);
+Vector Schmidt(Vector a1,Vector a2);
 
+static int current_time;
+static int border;
+static int boss_warning;
+static int devour_warning;
+static double boss_r = (double)2000;
+static Vector speed;
+static Position last_pos;
+static Position Devour[10];//记录Devour的位置
 
-
-static deque<AR_VECTOR> queue;
-static AR_VECTOR e(0,0,0);//speed
-static vector<AR_VECTOR> vec;
-static list<int>test;
-static bool border=false;
-static bool center = false;
 void AIMain() {
-	int i = 0;
-	int obj=-1;
-	int boss = -1;
-	e = AR_VECTOR(0, 0, 0);
-	AR_GAME G;
-	queue.clear();
-	vec.clear();
-	if (!border) {
-		if (G.position().border(1.2*G.radius())&LEFT) {
-			e = e + AR_VECTOR(100, 0, 0);
-			border = true;
-		}
-		else if (G.position().border(1.2*G.radius())&RIGHT) {
-			e = e - AR_VECTOR(100, 0, 0);
-			border = true;
-		}
-		if (G.position().border(1.2*G.radius())&UP) {
-			e = e - AR_VECTOR(0, 0, 100);
-			border = true;
-		}
-		else if (G.position().border(1.2*G.radius())&DOWN) {
-			e = e + AR_VECTOR(0, 0, 100);
-			border = true;
-		}
-		if (G.position().border(1.2*G.radius())&BEHIND) {
-			e = e + AR_VECTOR(0, 100, 0);
-			border = true;
-		}
-		else if (G.position().border(1.2*G.radius())&FRONT) {
-			e = e - AR_VECTOR(0, 100, 0);
-			border = true;
-		}
+	if (GetStatus() -> team_id) return;
+	srand(time(0));
+	for(;;){
+		printf("time=%d\thealth=%d\tability=%d\n",GetTime(),me[0].health,me[0].ability);
+		Init();
+		speed=MaximumSpeed(speed);
+		Move(me[0].id,speed);
+		//Strategy();
 	}
-	else {
-		if ((G.position().border(2.5*G.radius())&LEFT)) {
-			e = e + AR_VECTOR(100, 0, 0);
-			i++;
-		}
-		else if ((G.position().border(2.5*G.radius())&RIGHT)) {
-			e = e - AR_VECTOR(100, 0, 0);
-			i++;
-		}
-		if ((G.position().border(2.5*G.radius())&UP)) {
-			e = e - AR_VECTOR(0, 0, 100);
-			i++;
-		}
-		else if ((G.position().border(2.5*G.radius())&DOWN)) {
-			e = e + AR_VECTOR(0, 0, 100);
-			i++;
-		}
-		if ((G.position().border(2.5*G.radius())&BEHIND)) {
-			e = e + AR_VECTOR(0, 100, 0);
-			i++;
-		}
-		else if ((G.position().border(2.5*G.radius())&FRONT)) {
-			e = e - AR_VECTOR(0, 100, 0);
-			i++;
-		}
-		if (!i) border = false;
-	}
-	if (!border) {
-		if (G.position().length() < 2000) {
-			e = G.position();center = true;
-		}
-		else if (G.position().length() > 6000) {
-			e = AR_VECTOR(0, 0, 0) - G.position();center = true; 
-		}
-	}
-	cout << border << "\tSEP\t" << e;
-	for (int i = 0;i < G.object_number;i++) {
-		switch (G.check(G[i])) {
-		case ENERGY: {
-			if (((center || border)&&((G.position(G[i]) ^ e)>0.7))||(!center)||(!border)) {
-				if (G.distance(G[i]) < 3 * G.radius()) queue.push_back(G[i].pos);
-			}
-			break;
-		}
-		case ADVANCED_ENERGY: {
-			if (((center || border)&&((G.position(G[i]) ^ e)>0.3)) ||( !center)||(!border)) {
-				if (G.distance(G[i]) < 5 * G.radius()) queue.push_back(G[i].pos);
-			}
-			break;
-		}
-		case DEVOUR: {
-			vec.push_back(G.position(G[i]));
-			break;
-		}
-		case 250: {
-			obj = i;
-			break;
-		}
-		}
-	}
-	
-	while (true) {
-		if (queue.empty()) break;
-		if ((queue.front() - G.position()).length() < 0.8*G.radius()) queue.pop_front();
-		if (queue.empty()) break;
-		if((queue.front()^G.position())<-0.5) queue.pop_front();
-		else break;
-	}
-	G.Update(LONG_ATTACK);
-	if (boss >= 0) {
-		if (G.radius() > G.radius(G[boss])) {
-			G.MoveTo(G.position(G[boss]));
-			return;
-		}
-		else {
-			if (G.distance(G[boss]) < G.radius() * 3) {
-				G.MoveInSPEED(G.position() - G.position(G[boss]));
-			}
-		}
-	}
-	if (obj >= 0) {
-		if (G.radius() > G.radius(G[obj])) {
-			G.MoveTo(G.position(G[obj]));
-			return;
-		}
-	}
-	if (obj >= 0 ) {
-		if (G.distance(G[obj]) < kLongAttackRange[G.skill_level(LONG_ATTACK)]) {
-		cout << "Long Attack Ready" << endl;
-		G.long_attack(G[obj]);
-		return;
-		}
-	}
-	if (border || queue.empty()) {
-		G.MoveInSPEED(e);return;
-	}
-	else G.MoveTo(queue.front());
-	return;
 }
 
+void Init(){
+	map = GetMap();
+	current_time = map -> time;
+	register int i;
+	register double border_r=0.5*me[0].radius;
+	me[0] = GetStatus() -> objects[0];
+	opponent_number = devour_number = see_boss = 0;
+	speed.x = speed.y = speed.z = (double)0;
 
-
-
-
-
-
-AR_VECTOR::AR_VECTOR() {
-	x = (kMapSize >> 1);
-	y = (kMapSize >> 1);
-	z = (kMapSize >> 1);
+	for (i = 0; i < map -> objects_number; ++i)
+	{
+		register double F = (double)0, dis = dist(map -> objects[i].pos, me[0].pos);
+		switch (map -> objects[i].type){
+			case PLAYER:
+				if (map -> objects[i].team_id == GetStatus() -> team_id) break;
+				opponent[opponent_number++] = map -> objects[i];
+				/*if (me[0].radius < map -> objects[i].radius) 
+					F = -10 * me[0].radius * map -> objects[i].radius / POW(dis, 3);
+				if (me[0].radius > map -> objects[i].radius) 
+					F = 30 * me[0].radius * map -> objects[i].radius / POW(dis, 3);*/
+				break;
+			case ENERGY:
+				F = 5 * POW(me[0].radius, 2) / POW(dis, 3);
+				if (IsBorder(border_r,map -> objects[i].pos))//如果到边界的距离小于r，力为0
+					F=0;
+				break;
+			case ADVANCED_ENERGY:
+				F = 8 * POW(me[0].radius, 2) / POW(dis, 3);
+				if (IsBorder(border_r,map -> objects[i].pos))//如果到边界的距离小于r，力为0
+					F=0;
+				break;
+			case DEVOUR:
+				Devour[devour_number]=map -> objects[i].pos;
+				devour_number++;
+				break;
+			case BOSS:
+				boss = map -> objects[i];
+				see_boss = 1;
+				boss_r = boss.radius;
+				boss_warning = (int)(dis < 1.2 * boss_r);
+				break;
+			default:
+				break;
+		}
+		Vector a2=Minus(map -> objects[i].pos,me[0].pos);
+		speed=Add(speed,Multiple(F,a2));
+	}
+	speed.x *= 100 + rand() % 10;
+	speed.y *= 100 + rand() % 10;
+	speed.z *= 100 + rand() % 10;
+	FBorder(1.1*me[0].radius);//如果碰到边界，速度置0
+	for (i=0;i<devour_number;i++)
+	{
+		if (IsDevour(1.1*me[0].radius,Devour[i]))//如果会碰到devour，速度正交化
+		{
+			Vector a2=Minus(Devour[i],me[0].pos);
+			//printf("prespeed=%.2f %.2f %.2f\n",speed.x,speed.y,speed.z);
+			speed=Schmidt(speed,a2);
+			//printf("currentspeed=%.2f %.2f %.2f\n",speed.x,speed.y,speed.z);
+		}
+	}
+	if (boss_warning && IsBoss(1.5*boss_r,boss.pos))
+	{
+		Vector a2=Minus(boss.pos,me[0].pos);
+		speed=Schmidt(speed,a2);
+	}
 }
-AR_VECTOR::AR_VECTOR(double a, double b, double c) {
-	x = a;
-	y = b;
-	z = c;
-}
-double AR_VECTOR::length()
+void Strategy()
 {
-	return sqrt((this->x)*(this->x)+(this->y)*(this->y)+(this->z)*(this->z));
+	if (opponent_number)
+	{
+		if (!opponent[0].shield_time)
+		{
+			if (dist(me[0].pos, opponent[0].pos) - me[0].radius - opponent[0].radius < kShortAttackRange[me[0].skill_level[SHORT_ATTACK]]) 
+				short_attack(me[0]);
+			if (dist(me[0].pos, opponent[0].pos) - me[0].radius - opponent[0].radius < kLongAttackRange[me[0].skill_level[LONG_ATTACK]]) 
+				long_attack(me[0], opponent[0]);
+		}
+		else shield(me[0]);
+	}
+	upgrade(me[0], SHORT_ATTACK);
+	upgrade(me[0], HEALTH_UP);
+	if (me[0].skill_level[SHORT_ATTACK] >= me[0].skill_level[LONG_ATTACK])
+		upgrade(me[0], LONG_ATTACK);
+	if (!me[0].skill_level[SHIELD]) upgrade(me[0], SHIELD);
 }
-int AR_VECTOR::border(double d)
+int IsDevour(double d,Position des)//判断下一时刻会不会碰到吞噬者
+{
+	int flag=0;
+	Position Next=Add(me[0].pos,MaximumSpeed(speed)); 
+	if (dist(Next,des)<d)
+		flag=1;
+	return flag;
+}
+int IsBoss(double d,Position des)
+{
+	int flag=0;
+	Position Next=Add(me[0].pos,MaximumSpeed(speed)); 
+	if (dist(Next,des)<d)
+		flag=1;
+	return flag;
+}
+int IsBorder(double d,Position des)//判断物体是否在边界旁
 {
 	int R = NONE;
-	if (this->x < d) R |= LEFT;
-	else if (kMapSize - this->x < d) R |= RIGHT;
-	if (this->y < d) R |= BEHIND;
-	else if (kMapSize - this->y < d) R |= FRONT;
-	if (this->z < d) R |= DOWN;
-	else if (kMapSize - this->z < d) R |= UP;
+	if (des.x < d) R |= LEFT;
+	else if (kMapSize - des.x < d) R |= RIGHT;
+	if (des.y < d) R |= BEHIND;
+	else if (kMapSize - des.y < d) R |= FRONT;
+	if (des.z < d) R |= DOWN;
+	else if (kMapSize - des.z < d) R |= UP;
 	return R;
 }
-Position AR_VECTOR::cast()
+int FBorder(double r)//判断下一时刻会不会碰到边界，如果会，速度置0；
 {
-	return{ this->x, this->y, this->z };
+	register int flag = NONE;
+	if (me[0].pos.x<r && speed.x<0) 
+		speed.x=-0,flag=LEFT;
+	if (me[0].pos.x > kMapSize - r && speed.x>0) 
+		speed.x=-0,flag=RIGHT;
+	if (me[0].pos.y<r && speed.y<0) 
+		speed.y=-0,flag=BEHIND;
+	if (me[0].pos.y > kMapSize - r && speed.y>0) 
+		speed.y=-0,flag=FRONT;
+	if (me[0].pos.z<r && speed.z<0) 
+		speed.z=-0,flag=DOWN;
+	if (me[0].pos.z > kMapSize - r && speed.z>0) 
+		speed.z=-0,flag=UP;
+	return flag;
 }
-void AR_VECTOR::operator=(AR_VECTOR &A)
+Vector MaximumSpeed(Vector vec){
+	register double len = length(vec);
+	vec.x *= (kMaxMoveSpeed + kDashSpeed[me[0].skill_level[DASH]]) / len;
+	vec.y *= (kMaxMoveSpeed + kDashSpeed[me[0].skill_level[DASH]]) / len;
+	vec.z *= (kMaxMoveSpeed + kDashSpeed[me[0].skill_level[DASH]]) / len;
+	return vec;
+}
+int cost(PlayerObject obj, SkillType skill){
+	if (obj.skill_level[skill]) return kBasicSkillPrice[skill] << obj.skill_level[skill];
+	register int i, cnt = 0;
+	for (i = 0; i < kSkillTypes; ++i)
+		if (obj.skill_level[i]) ++cnt;
+	return kBasicSkillPrice[skill] << cnt;
+}
+
+void upgrade(PlayerObject obj, SkillType skill){
+	if (obj.skill_level[skill] == 5) return;
+	if (obj.ability < cost(obj, skill)) return;
+	UpgradeSkill(obj.id, skill);
+}
+
+void long_attack(PlayerObject obj, Object target){
+	if (!obj.skill_level[LONG_ATTACK]) return;
+	if (obj.skill_cd[LONG_ATTACK]) return;
+	if (dist(obj.pos, target.pos) - obj.radius - target.radius > kLongAttackRange[obj.skill_level[LONG_ATTACK]]) 
+		return;
+	LongAttack(obj.id, target.id);
+}
+
+void short_attack(PlayerObject obj){
+	if (!obj.skill_level[SHORT_ATTACK]) return;
+	if (obj.skill_cd[SHORT_ATTACK]) return;
+	ShortAttack(obj.id);
+}
+
+void shield(PlayerObject obj){
+	if (!obj.skill_level[SHIELD]) return;
+	if (obj.skill_cd[SHIELD]) return;
+	Shield(obj.id);
+}
+
+void dash(PlayerObject obj){
+	if (!obj.skill_level[DASH]) return;
+	if (obj.skill_cd[DASH]) return;
+	Dash(obj.id);
+}
+
+double dist(Position src, Position des){
+	register Position vec;
+	vec.x = des.x - src.x;
+	vec.y = des.y - src.y;
+	vec.z = des.z - src.z;
+	return length(vec);
+}
+
+double length(Vector vec){
+	return sqrt(dot_product(vec, vec));
+}
+
+double dot_product(Vector vec1, Vector vec2){
+	return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+}
+
+double ABS(double x){
+	return x > 0 ? x : -x;
+}
+
+double SQR(double x){
+	return x * x;
+}
+
+double POW(double x, int a){
+	if (ABS(x) < eps) x = 0.1;
+	register double res = (double)1;
+	for (; a; a >>= 1, x = SQR(x))
+		if (a & 1) res *= x;
+	return res;
+}
+Vector Add(Vector a1,Vector a2){
+	Vector temp;
+	temp.x=a1.x+a2.x;
+	temp.y=a1.y+a2.y;
+	temp.z=a1.z+a2.z;
+	return temp;
+}
+Vector Minus(Vector a1,Vector a2)//a1-a2
 {
-	this->x = A.x;
-	this->y = A.y;
-	this->z = A.z;
+	Vector temp;
+	temp.x=a1.x-a2.x;
+	temp.y=a1.y-a2.y;
+	temp.z=a1.z-a2.z;
+	return temp;
 }
-void AR_VECTOR::operator=(Position P)
+Vector Multiple(double k,Vector a)
 {
-	x = P.x;
-	y = P.y;
-	z = P.z;
+	Vector temp;
+	temp.x=k*a.x;
+	temp.y=k*a.y;
+	temp.z=k*a.z;
+	return temp;
 }
-AR_VECTOR::AR_VECTOR(Position p) {
-	x=p.x;
-	y=p.y;
-	z=p.z;
-}
-AR_VECTOR operator+(const AR_VECTOR&A, const AR_VECTOR&B) {
-	return AR_VECTOR(A.x + B.x, A.y + B.y, A.z + B.z);
-}
-AR_VECTOR operator-(const AR_VECTOR&A, const AR_VECTOR&B) {
-	return AR_VECTOR(A.x - B.x, A.y - B.y, A.z - B.z);
-}
-double operator,(const AR_VECTOR & A, const AR_VECTOR & B)
+Vector Schmidt(Vector a1,Vector a2)//由a1生成一个与a2垂直的向量 
 {
-	return (A.x*B.x + A.y*B.y + A.z*B.z);
+	Vector temp1,temp2;
+	temp2=Multiple(dot_product(a1,a2)/dot_product(a2,a2),a2); 
+	temp1=Minus(a1,temp2);
+	return temp1;
 }
-ostream & operator<<(ostream & os, AR_VECTOR & p)
-{
-	os << p.x << '\t' << p.y << '\t' << p.z << '\t' << p.length() << endl;
-	return os;
-}
-AR_VECTOR operator*(const AR_VECTOR&A, const double k) {
-	return AR_VECTOR(A.x*k, A.y*k, A.z*k);
-}
-AR_VECTOR operator*(const double k, const AR_VECTOR& A) {
-	return (A*k);
-}
-double operator^(AR_VECTOR &A, AR_VECTOR &B)
-{
-	return (A, B) / (A.length()*B.length());
-}
-int operator==(const AR_VECTOR &A, const AR_VECTOR &B)
-{
-	return (A - B).length() < 0.01;
-}
-AR_VECTOR operator*(const AR_VECTOR& A, const AR_VECTOR& B) {
-	return AR_VECTOR(A.y*B.z - A.z*B.y, A.z*B.x - A.x*B.z, A.x*B.y - A.y * B.x);
-}
-AR_GAME::AR_GAME()
-{
-	map = GetMap();
-	myteam = GetStatus()->team_id;
-	number = GetStatus()->objects_number;
-	object_number = map->objects_number;
-}
-int AR_GAME::MoveInSPEED(AR_VECTOR P, int i)
-{
-	PlayerObject p = GET(i);
-	AR_VECTOR Q = P*((kMaxMoveSpeed + kDashSpeed[p.skill_level[DASH]]) / P.length());
-	if (! (Q== p.speed)) {
-		Move(p.id, Q.cast());
-		return 0;
-	}
-	else return -1;
-}
-int AR_GAME::MoveTo(AR_VECTOR p,int i)
-{
-	PlayerObject my = GET(i);
-	if (p == my.pos) return -1;
-	double k = ((kMaxMoveSpeed + kDashSpeed[my.skill_level[DASH]]) / (p - my.pos).length());
-	AR_VECTOR a = (k*(p - my.pos));
-	return MoveInSPEED(a,i);
-}
-Object  AR_GAME::operator[](int n)
-{
-	return map->objects[n];
-}
-PlayerObject AR_GAME::GET(int n)
-{
-	return GetStatus()->objects[n];
-}
-double AR_GAME::distance(Object aim, int i)
-{
-	return (position(i) - position(aim)).length();
-}
-int AR_GAME::cost(SkillType skill,int i) {
-	PlayerObject my = GET(i);
-	if (my.skill_level[skill]) {
-		return (kBasicSkillPrice[skill] << my.skill_level[skill]);
-	}
-	else {
-		int i, count;
-		for (i = 0, count = 0;i < kSkillTypes;i++) {
-			if (my.skill_level[i]) count++;
-		}
-		return(kBasicSkillPrice[skill] << count);
-	}
-}
-int AR_GAME::Update(SkillType skill,int i)
-{
-	PlayerObject my = GET(i);
-	if (my.ability >= cost(skill,i)) {
-		UpgradeSkill(my.id, skill);
-		return 0;
-	}
-	else return -1;
-}
-int AR_GAME::long_attack(Object aim,int i) {
-	PlayerObject my = GET(i);
-	if (my.skill_level[LONG_ATTACK] && my.skill_cd[LONG_ATTACK] == 0
-		&& (AR_VECTOR(my.pos)-AR_VECTOR(aim.pos)).length()<=kLongAttackRange[my.skill_level[LONG_ATTACK]]) {
-		LongAttack(my.id, aim.id);
-		printf("LONG ATTACK AT\t%d\tLEVEL\t%d\n", GetTime(), my.skill_level[LONG_ATTACK]);
-		return 0;
-	}
-	else return -1;
-}
-int AR_GAME::short_attack(int i) {
-	PlayerObject my = GET(i);
-	if (my.skill_level[SHORT_ATTACK] && (my.skill_cd[SHORT_ATTACK] == 0)) {
-		ShortAttack(my.id);
-		printf("SHORT ATTACK AT\t%d\tLEVEL\t%d\n", GetTime(), my.skill_level[SHORT_ATTACK]);
-		return 0;
-	}
-	else {
-		return -1;
-	}
-}
-int AR_GAME::shield(int i) {
-	PlayerObject my = GET(i);
-	if (my.skill_level[SHIELD] && my.skill_cd[SHIELD] == 0) {
-		Shield(my.id);
-		printf("SHIELD AT\t%d\tLEVEL\t%d\n", GetTime(), my.skill_level[SHIELD]);
-		return 0;
-	}
-	else return -1;
-}
-int AR_GAME::health(int i)
-{
-	PlayerObject my = GET(i);
-	return my.health;
-}
-int AR_GAME::maxhealth(int i)
-{
-	PlayerObject my = GET(i);
-	return my.max_health;
-}
-int AR_GAME::vision(int i)
-{
-	PlayerObject my = GET(i);
-	return my.vision;
-}
-double AR_GAME::radius(int i)
-{
-	PlayerObject my = GET(i);
-	return my.radius;
-}
-double AR_GAME::radius(Object aim)
-{
-	return aim.radius;
-}
-AR_VECTOR AR_GAME::speed(int i)
-{
-	PlayerObject my = GET(i);
-	return AR_VECTOR(my.speed);
-}
-AR_VECTOR AR_GAME::position(int i)
-{
-	PlayerObject my = GET(i);
-	return AR_VECTOR(my.pos);
-}
-AR_VECTOR AR_GAME::position(Object aim)
-{
-	return AR_VECTOR(aim.pos);
-}
-int AR_GAME::long_attack_casting(int i)
-{
-	PlayerObject my = GET(i);
-	return my.long_attack_casting;
-}
-int AR_GAME::long_attack_casting(Object aim)
-{
-	return aim.long_attack_casting;
-}
-int AR_GAME::shield_time(int i)
-{
-	PlayerObject my = GET(i);
-	return my.shield_time;
-}
-int AR_GAME::shield_time(Object aim)
-{
-	return aim.shield_time;
-}
-int AR_GAME::dash_time(int i)
-{
-	PlayerObject my = GET(i);
-	return my.dash_time;
-}
-int AR_GAME::ability(int i)
-{
-	PlayerObject my = GET(i);
-	return my.ability;
-}
-int AR_GAME::skill_level(SkillType skill,int i)
-{
-	PlayerObject my = GET(i);
-	return my.skill_level[skill];
-}
-int AR_GAME::skill_cd(SkillType skill,int i)
-{
-	PlayerObject my = GET(i);
-	return my.skill_cd[skill];
-}
-int AR_GAME::check(Object aim)
-{
-	if (aim.type != PLAYER) return aim.type;
-	else {
-		if (aim.team_id == myteam) return 88;
-		else return 250;
-	}
-}
-int AR_GAME::dash(int i) {
-	PlayerObject my = GET(i);
-	if (my.skill_level[DASH] && my.skill_cd[DASH] == 0) {
-		Dash(my.id);
-		printf("%d\tDASH AT\t\t%d\tLEVEL\t%d\n", my.id, GetTime(), my.skill_level[DASH]);
-		return 0;
-	}
-	else return -1;
-} 
